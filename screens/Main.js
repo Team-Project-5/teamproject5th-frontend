23821cc8efa2da1402412cf18a48938874c4c55a
Jake-Kim38 <kjm25034841@gmail.com>
2023-11-12 02:10:05 +0900

    feat: 메인 페이지 디자인 bottom navigator 제외 구현

---
+++ screens/Main.js
@@ -0,0 +1,125 @@
+import styled from "styled-components/native";
+import { Fontisto } from "@expo/vector-icons";
+import { StyleSheet, Image, Dimensions, Text } from "react-native";
+import * as Font from "expo-font";
+import { useCallback } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const screenWidth = Dimensions.get("window").width;
+
+const MainPage = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        Jalnan: require("../assets/fonts/Jalnan2TTF.ttf"),
+      });
+      setFontsLoaded(true);
+    }
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <PageArea>
+      <TitleContainer>
+        <TitleText>역이어때</TitleText>
+        <Text style={styles.search}>
+          <Fontisto name="search" size={24} color="black" />
+        </Text>
+      </TitleContainer>
+      <Image
+        style={styles.image}
+        source={require("../assets/images/Metro.png")}
+        resizeMode="contain"
+      />
+      <MainButton>
+        <UpsideTitle>오늘,</UpsideTitle>
+        <DownsideTitleArea>
+          <DownsideMainTitle>서울역 어때?</DownsideMainTitle>
+          <DownsideSubTitle>-전체 게시글 모아보기-</DownsideSubTitle>
+        </DownsideTitleArea>
+      </MainButton>
+    </PageArea>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: screenWidth,
+    height: 407,
+  },
+  search: {
+    marginRight: 21,
+  },
+});
+
+const PageArea = styled.View`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const TitleContainer = styled.View`
+  width: ${screenWidth}px;
+  height: 42px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 50px;
+`;
+
+const TitleText = styled.Text`
+  font-size: 36px;
+  font-weight: bold;
+  margin-left: 22px;
+  color: #f8243c;
+  font-family: Jalnan;
+`;
+
+const MainButton = styled.TouchableOpacity`
+  width: 350px;
+  height: 158px;
+  background-color: #f8243c;
+  border-radius: 20px;
+  box-shadow: 0px 0px 4px #f8243c;
+  display: flex;
+  justify-content: center;
+`;
+
+const UpsideTitle = styled.Text`
+  font-size: 25px;
+  font-weight: 600;
+  margin-left: 22px;
+  color: white;
+  font-family: Jalnan;
+`;
+
+const DownsideTitleArea = styled.View`
+  width: 240px;
+  height: 55px;
+  margin-left: 98px;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const DownsideMainTitle = styled.Text`
+  font-size: 42px;
+  font-weight: 600;
+  color: white;
+  font-family: Jalnan;
+`;
+
+const DownsideSubTitle = styled.Text`
+  font-size: 12px;
+  font-weight: bold;
+  color: white;
+  box-shadow: 0px 0px 4px #000;
+`;
+
+export default MainPage;
