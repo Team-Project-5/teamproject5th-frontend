c1fa0972fac942c3eb92eac3dded045fa376213e
MINJULEE <leeminju913@gmail.com>
2023-11-15 17:29:59 +0900

    fix: 충돌 해결

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import React from "react";
-import Scrape from "./screens/Scrape";
-import HotStation from "./screens/HotStation";
-import Profile from "./screens/Profile";
-import Login from "./screens/Login";
+import { StyleSheet, Text, View } from "react-native";
+import MainPage from "./screens/Main";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Login />
+      <MainPage />
       <StatusBar style="auto" />
     </View>
   );
