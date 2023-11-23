aa1818cb7024d485d27158bbf80116735a6d1ce6
Jake-Kim38 <kjm25034841@gmail.com>
2023-11-24 00:29:49 +0900

    fix: 로그인 상단 버튼 동작 구현

--- components/Head.js
+++ components/Head.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import {Text,View} from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
-const Head = () => ( //다른 화면들까지 바꿔서 해야할듯
-    <View>
-        <Text style = {{ fontSize: 36 }}>환영합니다!</Text>
-        <Text style = {{ fontSize: 17, color: '#6A798A'}}>회원가입 또는 로그인을 해주세요</Text>
-    </View>
+const Head = () => (
+  //다른 화면들까지 바꿔서 해야할듯
+  <View style={styles.view}>
+    <Text style={{ fontSize: 36 }}>환영합니다!</Text>
+    <Text style={{ fontSize: 17, color: "#6A798A" }}>
+      회원가입 또는 로그인을 해주세요
+    </Text>
+  </View>
 );
+
+const styles = StyleSheet.create({
+  view: {
+    marginLeft: 20,
+    marginTop: 80,
+  },
+});
 
 export default Head;
